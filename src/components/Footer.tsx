ab173b4abf0e5d7e68cfd382aa302405c1fd5da4
John Hu <huziyong@gmail.com>
2026-02-01 21:44:29 +0800

    update github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -98,7 +98,7 @@ export default function Footer() {
              <span className="opacity-20">|</span>
              <Link href="/privacy" className="hover:text-foreground transition-colors no-underline">Privacy</Link>
              <span className="opacity-20">|</span>
-             <a href="https://github.com/vercel/next.js" target="_blank" rel="noreferrer" className="hover:text-foreground transition-colors no-underline">
+             <a href="https://github.com/hutusi/amytis" target="_blank" rel="noreferrer" className="hover:text-foreground transition-colors no-underline">
                Built with Amytis
              </a>
           </div>
